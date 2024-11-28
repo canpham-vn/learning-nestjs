56978c8d457a5bdb623122dc2062261b0a04a52d
canpham-vn <canpham0610@gmail.com>
2024-11-28 21:28:01 +0700

    One to many relationship

--- src/entities/property.entity.ts
+++ src/entities/property.entity.ts
@@ -2,10 +2,12 @@ import {
   Column,
   Entity,
   JoinColumn,
+  ManyToOne,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { PropertyFeature } from './propertyFeature.entity';
+import { User } from './user.entity';
 
 @Entity()
 export class Property {
@@ -29,4 +31,8 @@ export class Property {
     { cascade: true },
   )
   propertyFeature: PropertyFeature;
+
+  @ManyToOne(() => User, (user) => user.properties)
+  @JoinColumn({ name: 'ownerId' })
+  user: User;
 }
