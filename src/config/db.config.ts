795eb67d16a02be8e42f2ff1647f4c8d95532b27
canpham-vn <canpham0610@gmail.com>
2024-12-09 21:11:44 +0700

    Using config service

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,14 +1,18 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import * as path from 'path';
+import { registerAs } from '@nestjs/config';
 
-export default (): PostgresConnectionOptions => ({
-  url: process.env.url,
-  type: 'postgres',
-  port: +process.env.port,
-  // --- can import entities end add them into entities config
-  // entities: [Property],
+export default registerAs(
+  'dbconfig.dev',
+  (): PostgresConnectionOptions => ({
+    url: process.env.url,
+    type: 'postgres',
+    port: +process.env.port,
+    // --- can import entities end add them into entities config
+    // entities: [Property],
 
-  // --- to automatically add entities, use this way:
-  entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
-});
+    // --- to automatically add entities, use this way:
+    entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),
+);
